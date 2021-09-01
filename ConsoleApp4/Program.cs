2c292aef45ff59eb7a9193cb56dd260485cc3281
Pallabi007 <pallabi.biswas@dell.com>
2021-09-01 12:51:18 +0530

    New changes

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp4
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Lets learn .Net 5!");
+            Console.WriteLine("Pushed to GITHub ...XO..XO");
             Console.ReadKey();
         }
     }
